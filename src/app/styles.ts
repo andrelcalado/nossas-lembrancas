2b1e8a99a48ab54f0079fd8e347c97c23df0718e
andrelcalado <andrelcalad@hotmail.com>
2024-09-26 07:54:14 -0300

    :rocket: [FEAT] Library styled components configuration

---
+++ src/app/styles.ts
@@ -0,0 +1,11 @@
+'use client'
+
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 2.5em;
+  font-weight: 500;
+  line-height: 1.2;
+  margin: 0 0 0.5em 0;
+  color: red;
+`;

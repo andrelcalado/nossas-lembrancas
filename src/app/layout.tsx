15fc8371d810137eb4e1a35584f2ecac64ca887f
andrelcalado <andrelcalad@hotmail.com>
2024-10-19 17:36:19 -0300

    :bug: [FIX] iPhone zoom limit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,6 +66,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       <body className={Poppins.className}>
         <main>
           <StyledComponentsRegistry>

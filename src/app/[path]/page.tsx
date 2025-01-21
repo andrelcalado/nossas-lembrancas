d948cd87c46c7400d7846399566881aab964a380
andrelcalado <andrelcalad@hotmail.com>
2025-01-20 23:24:28 -0300

    :bug: [FIX] Open graph in server side

--- src/app/[path]/page.tsx
+++ src/app/[path]/page.tsx
@@ -1,9 +1,15 @@
 // Core
+import type * as next from "next"
 import React from 'react';
 import Head from 'next/head';
 
 // Layout
 import PageContent from './pageContent';
+
+export const metadata: next.Metadata = {
+  title: "Preparei algo especial para você!",
+  description: "Este presente é sobre nós. Clique e veja momentos que marcaram nossa história.",
+};
 
 export default function UserPage({ params }: { params: { path: string } }) {
   const decodedPath = decodeURIComponent(params.path);
@@ -11,18 +17,12 @@ export default function UserPage({ params }: { params: { path: string } }) {
   return (
     <>
       <Head>
-        <meta property="og:title" content="Preparei algo especial para você!" />
-        <meta
-          property="og:image"
-          content="https://i.ibb.co/PtQ1Fs0/OG.jpg"
-        />
-        <meta
-          property="og:description"
-          content="Este presente é sobre nós. Clique e veja momentos que marcaram nossa história."
-        />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="627" />
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
         <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://nossas-lembrancas.vercel.app" />
       </Head>
 
       <PageContent path={decodedPath} />      

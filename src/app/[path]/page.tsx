527442ff91987d39f0b0a26da9f12021b591368d
andrelcalado <andrelcalad@hotmail.com>
2025-01-20 22:39:20 -0300

    :rocket: [FEAT] Gift page with open graph specifications

--- src/app/[path]/page.tsx
+++ src/app/[path]/page.tsx
@@ -2,6 +2,7 @@
 
 // Core
 import React from 'react';
+import Head from 'next/head';
 
 // Components
 import PreviewModal from '@/components/PreviewModal';
@@ -14,15 +15,33 @@ export default function UserPage({ params }: { params: { path: string } }) {
   const decodedPath = decodeURIComponent(params.path);
   const { coupleTimeline } = usePath(decodedPath);
 
-  return coupleTimeline ? (
-    <PreviewModal
-      openModal
-      isGift
-      timelineData={coupleTimeline}
-      setOpenModal={() => {}}
-      handleToGift={() => {}}
-    />
-  ) : (
-    <Loading loading={!coupleTimeline} type="screen" />
-  );
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="Preparei algo especial para você!" />
+        <meta
+          property="og:image"
+          content="https://i.ibb.co/PtQ1Fs0/OG.jpg"
+        />
+        <meta
+          property="og:description"
+          content="Este presente é sobre nós. Clique e veja momentos que marcaram nossa história."
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="627" />
+        <meta property="og:type" content="website" />
+      </Head>
+
+      {coupleTimeline ? (
+        <PreviewModal
+          openModal
+          isGift
+          timelineData={coupleTimeline}
+          setOpenModal={() => {}}
+          handleToGift={() => {}}
+        />
+      ) : (
+        <Loading loading={!coupleTimeline} type="screen" />
+      )}
+  </>)
 }
